46dc9692d55c1a5561eb312e134a9917aec3a8d7
Ahtisham <ahtishamshafi9906@gmail.com>
2021-10-09 20:34:26 +0530

    Add django decouple

--- detoxlab/settings/components/common.py
+++ detoxlab/settings/components/common.py
@@ -1,14 +1,10 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1ylv91xzf^0x4#421%sv#n0046b1sosdj)pjzc6n=g*=!)z3z)'
+SECRET_KEY = config('SECRET_KEY')
 
 # Application definition
 
