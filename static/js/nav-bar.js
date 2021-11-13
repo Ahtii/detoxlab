f2b4aff37391a1f2b7d9aed046b75bdb1d6ab84e
Ahtisham <ahtishamshafi9906@gmail.com>
2021-11-13 23:46:39 +0530

    update layout

--- static/js/nav-bar.js
+++ static/js/nav-bar.js
@@ -1,3 +1,26 @@
+$(function(){
+	$(document).scroll(function(){
+		var $nav = $(".fixed-top");
+		var $navbar = $(".navbar");
+		var $navlink = $(".nav-link");
+		var $logo_brand = $(".navbar-brand");
+		var $img = $(".navbar-brand img");
+		if ($(this).scrollTop() > $nav.height()){
+			$nav.toggleClass('scrolled', true);
+			$logo_brand.toggleClass('scrolled', true);
+			$navbar.toggleClass('shadow', true);
+			$img.attr('src', '/static/media/images/logos/branding_logo_updated.png')
+			$navlink.toggleClass('scrolled', true);
+		}else{
+			$nav.toggleClass('scrolled', false);
+			$logo_brand.toggleClass('scrolled', false);
+			$navbar.toggleClass('shadow', false);
+			$img.attr('src', '/static/media/images/logos/branding_logo_updated_white.png');
+			$navlink.toggleClass('scrolled', false);
+		}
+	});
+});
+
 $(document).ready(function(){
     console.log("working");
     if ($(window).width() <= 754)
